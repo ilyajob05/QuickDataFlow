8382b87800e90446e56b3e8cc48c25a5fbfefc74
ilyajob05 <ilyajob05@gmail.com>
2022-01-29 19:40:56 +0300

    Update codecov.yml and get_code_cov.sh

--- get_code_cov.sh
+++ get_code_cov.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-for filename in `find . | egrep '\.cpp'`;
-do
-  gcov -n -o . $filename > /dev/null;
-done
+#for filename in `find . | egrep '\.cpp'`;
+#do
+#  gcov -n -o . $filename > /dev/null;
+#done
+
+gcovr -r .
+gcovr -r . --html -o coverage.html
